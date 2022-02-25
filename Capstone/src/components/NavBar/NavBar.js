8fe8bceb16e575d690bbf1953fb0d1d70af09f36
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-25 09:22:08 -0900

    add navbar, needs styling

--- Capstone/src/components/NavBar/NavBar.js
+++ Capstone/src/components/NavBar/NavBar.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap'
 
 function NavBar() {
-
-    return(
-        <React.Fragment>
-            {/*Nav Buttons, maybe make seperate */}
-        </React.Fragment>
-    )
+	const navStyles = {
+		display: "flex"
+	}
+	
+  return(
+		<React.Fragment>
+			<div style={navStyles}>
+				<Link to="/">
+					<Button>
+						<h2>Playable Portfolio</h2>
+					</Button>
+				</Link>
+				<Link to="/portfolio">
+					<Button>
+						<h2>Standard Portfolio</h2>
+					</Button>
+				</Link>
+				<Link to="/about">
+					<Button>
+						<h2>About Me</h2>
+					</Button>
+				</Link>
+				<Link to="/contact">
+					<Button>
+						<h2>Contact</h2>
+					</Button>
+				</Link>
+			</div>
+		</React.Fragment>
+  )
 }
 
 export default NavBar;

488f64058d519aa1f6d9a5ba1efbfccf3abeabef
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-25 08:18:18 -0900

    add routing

--- Capstone/src/components/App.js
+++ Capstone/src/components/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import UnityGame from './UnityGame';
+import NavBar from './NavBar/NavBar';
+import About from './About';
+import Contact from './Contact'
+import PortfolioControl from './StandardPortfolio/PortfolioControl';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
-   <React.Fragment>
-    <UnityGame/>
-   </React.Fragment>
+    <Router>
+      <Container>
+        <NavBar/>
+        <Switch>
+          <Route path='/'>
+            <UnityGame/>
+          </Route>
+          <Route path='portfolio'>
+            <PortfolioControl/>
+          </Route>
+          <Route path='about'>
+            <About/>
+          </Route>
+          <Route path='contact'>
+            <Contact/>
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 

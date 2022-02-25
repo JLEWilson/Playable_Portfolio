c720005fc4bf580debcba067142963ff16c5088e
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-25 09:24:31 -0900

    add routs

--- Capstone/src/components/App.js
+++ Capstone/src/components/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import UnityGame from './UnityGame';
-import NavBar from './NavBar/NavBar';
-import About from './About';
-import Contact from './Contact'
-import PortfolioControl from './StandardPortfolio/PortfolioControl';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
+import React from "react";
+import UnityGame from "./UnityGame";
+import NavBar from "./NavBar/NavBar";
+import About from "./About";
+import Contact from "./Contact"
+import PortfolioControl from "./StandardPortfolio/PortfolioControl";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
@@ -13,17 +13,17 @@ function App() {
       <Container>
         <NavBar/>
         <Switch>
-          <Route path='/'>
-            <UnityGame/>
+          <Route exact path="/">
+            <UnityGame />
           </Route>
-          <Route path='portfolio'>
-            <PortfolioControl/>
+          <Route path="/portfolio">
+            <PortfolioControl />
           </Route>
-          <Route path='about'>
-            <About/>
+          <Route path="/about">
+            <About />
           </Route>
-          <Route path='contact'>
-            <Contact/>
+          <Route path="/contact">
+            <Contact />
           </Route>
         </Switch>
       </Container>

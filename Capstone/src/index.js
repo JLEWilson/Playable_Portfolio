06b976a9e275f456fe6b3230075d01aa1822ff08
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-25 15:47:18 -0900

    re add middleware

--- Capstone/src/index.js
+++ Capstone/src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
-import rootReducer from './reducers/index';
+import { createStore, applyMiddleware } from 'redux';
+import reducer from './reducers/repositories-reducer';
 import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import middlewareLogger from './middleware/middleware-logger';
 
-const store = createStore(rootReducer); // add reducer
+const store = createStore(reducer, applyMiddleware(thunkMiddleware, middlewareLogger));
+
 
 store.subscribe(() =>
   console.log(store.getState())

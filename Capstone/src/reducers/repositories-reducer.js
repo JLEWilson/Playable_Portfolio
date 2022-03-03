dbaef930e50998ffb7376cf56327e5d9bad314f4
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-03-03 13:30:01 -0900

    add states for retrieving repos

--- Capstone/src/reducers/repositories-reducer.js
+++ Capstone/src/reducers/repositories-reducer.js
@@ -1,25 +1,41 @@
 import * as c from './../actions/ActionTypes';
 
-const defaultState = {
-  isLoading: false,
-  repositories: [],
-  error: null
+let initialState = {
+  isLoadingUserInfo: false,
+  isLoadingRepositories: false,
+  userInfo: {},
+  error: null,
+  repositories: []
 }
 
-export default (state = defaultState, action) => {
-  switch(action.type) {
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case c.REQUEST_USER_INFO:
+      return Object.assign({}, state, {
+        isLoadingUserInfo: true
+      });
+    case c.GET_USER_INFO_SUCCESS:
+      return Object.assign({}, state, {
+        isLoadingUserInfo: false,
+        userInfo: action.userInfo
+      });
+    case c.GET_USER_INFO_FAILURE:
+      return Object.assign({}, state, {
+        isLoadingUserInfo: false,
+        error: action.error
+      });
     case c.REQUEST_REPOSITORIES:
       return Object.assign({}, state, {
-        isLoading: true
+        isLoadingRepositories: true,
       });
     case c.GET_REPOSITORIES_SUCCESS:
       return Object.assign({}, state, {
-        isLoading: false,
+        isLoadingRepositories: false,
         repositories: action.repositories
       });
     case c.GET_REPOSITORIES_FAILURE:
       return Object.assign({}, state, {
-        isLoading: false, 
+        isLoadingRepositories: false,
         error: action.error
       });
     default:

c0b2973887bb7bd664753c2eb31f5ecaf5cbf28d
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-03-03 13:28:52 -0900

    fix api call to correctly dispatch using redux thunk

--- Capstone/src/actions/index.js
+++ Capstone/src/actions/index.js
@@ -1,55 +1,55 @@
-import * as c from './ActionTypes'
+import * as c from './ActionTypes';
 
+export const requestUserInfo = () => ({
+  type: c.REQUEST_USER_INFO
+});
+export const getUserInfoSuccess = (userInfo) => ({
+  type: c.GET_USER_INFO_SUCCESS,
+  userInfo
+});
+export const getUserInfoFailure = (error) => ({
+  type: c.GET_USER_INFO_FAILURE,
+  error
+});
 export const requestRepositories = () => ({
   type: c.REQUEST_REPOSITORIES
 });
-
+export const getRepositoriesFailure = (error) => ({
+  type: c.GET_REPOSITORIES_FAILURE,
+  error
+});
 export const getRepositoriesSuccess = (repositories) => ({
   type: c.GET_REPOSITORIES_SUCCESS,
   repositories
 });
 
-export const getRepositoriesFailure = (error) => ({
-  type: c.GET_REPOSITORIES_FAILURE,
-  error
-});
-
-export const githubApiCall = () => {
+export const getAllRepositories = () => {
   return dispatch => {
-    dispatch(requestRepositories);
-    return fetch(`https://api.github.com/users/jlewilson/repos?per_page=60&page=1`)
-      .then(response => response.json())
-      .then(jsonifiedResponse => jsonifiedResponse.concat(checkForAdditionalRepositories(2, jsonifiedResponse)))
-      .then(
-        (allRepositories) => {
-          console.log(allRepositories);
-          if(Array.isArray(allRepositories)){
-            dispatch(
-              getRepositoriesSuccess(
-                allRepositories));
-          } else {
-            throw(allRepositories)
-          }
-        })
-      .catch((error) => {
-        dispatch(getRepositoriesFailure(error));
-      });
-  }
-}
-
-const checkForAdditionalRepositories = (pageNumber, currentRepositories) => {
-  console.log(currentRepositories);
-  if(currentRepositories.length === 0)
-  {
-    return [];
-  }  
-  fetch(`https://api.github.com/users/jlewilson/repos?per_page=60&page=${pageNumber}`)
+    dispatch(requestUserInfo);
+    const totalPages = fetch(`https://api.github.com/users/jlewilson`)
     .then(response => response.json())
-    .then(
-      (jsonifiedResponse) => {
-        return jsonifiedResponse.concat(checkForAdditionalRepositories(pageNumber + 1, jsonifiedResponse));
-      })
+    .then(json => {
+      dispatch(getUserInfoSuccess(json));
+      return Math.ceil(json["public_repos"]/60);
+    })
     .catch((error) => {
-      return error;
+      dispatch(getUserInfoFailure(error))
     });
+    
+    return Promise.all(
+      Array.from(
+        Array(totalPages),
+        (_, i) => fetch(`https://api.github.com/users/jlewilson/repos?per_page=60&page=${i+1}`)
+      )
+      ).then(
+        responses => Promise.all(responses.map(r => r.json()))
+      ).then(
+        (jsonifiedResponse) => {
+          dispatch(getRepositoriesSuccess(jsonifiedResponse.flat()))
+        }
+      ).catch((error) => {
+        dispatch(getRepositoriesFailure(error));
+      })
+  }
+  
 }

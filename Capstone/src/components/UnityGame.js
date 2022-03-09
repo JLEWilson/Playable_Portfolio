7c029e0898057edb8740696fff0b33e5c8065922
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-03-09 12:49:08 -0900

    Adjust game height based on screen size, using media querys and keeping aspect ratio

--- Capstone/src/components/UnityGame.js
+++ Capstone/src/components/UnityGame.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Unity, { UnityContext } from "react-unity-webgl";
+import { useMediaQuery } from '../Hooks/MediaQuery';
 
 const unityContext = new UnityContext({
   loaderUrl: "/unityBuild/PlayablePortfolio.loader.js",
@@ -7,12 +8,37 @@ const unityContext = new UnityContext({
   frameworkUrl: "/unityBuild/PlayablePortfolio.framework.js",
   codeUrl: "/unityBuild/PlayablePortfolio.wasm",
 });
-const gameStyle = {
-  width:"75vw",
-  alignSelf: "center"
-}
+
 function UnityGame() {
-  return <Unity style={gameStyle} unityContext={unityContext} />;
+  const isMDScreen = useMediaQuery('(min-width: 800px) and (max-width: 1199px)');
+  const isLGScreen = useMediaQuery('(min-width: 1200px) and (max-width: 1599px)');
+  const isXLScreen = useMediaQuery('(min-width: 1600px)');
+  
+  const style = {
+    game: (isMd, isLg, isXl) => ({
+      width:  isXl ? "1280px" :
+              isLg ? "960px" :
+              isMd ? "640px" : "320px",
+      height: isXl ? "720px" :
+              isLg ? "540px" :
+              isMd ? "360px" :"180px",
+      margin: "auto",
+      display: "block"
+		})
+    
+  }
+
+  return <Unity 
+          style={style.game(isMDScreen, isLGScreen, isXLScreen)} 
+          unityContext={unityContext} />
+  
 }
 
 export default UnityGame;
+
+/* aspect rations
+320 x 180, small
+640 x 360, medium
+960 x 540, large
+1280 x 720, largest
+*/

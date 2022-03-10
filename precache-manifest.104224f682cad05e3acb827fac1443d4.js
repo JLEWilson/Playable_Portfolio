8dabe23ca642b127e25324d9a3487fb3e722c1ab
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-03-09 15:42:22 -0900

    Updates

---
+++ precache-manifest.104224f682cad05e3acb827fac1443d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52df3bcf78da928261002eb153a884b2",
+    "url": "/playable_portfolio/index.html"
+  },
+  {
+    "revision": "0d5d3c250e5031c5913e",
+    "url": "/playable_portfolio/static/css/main.d9fb3009.chunk.css"
+  },
+  {
+    "revision": "d3d8748ac290b407da63",
+    "url": "/playable_portfolio/static/js/2.a271a3ae.chunk.js"
+  },
+  {
+    "revision": "46fe92a95a07403546af",
+    "url": "/playable_portfolio/static/js/3.b7f07be6.chunk.js"
+  },
+  {
+    "revision": "0d5d3c250e5031c5913e",
+    "url": "/playable_portfolio/static/js/main.db354acb.chunk.js"
+  },
+  {
+    "revision": "931058d92438d8b6b0cf",
+    "url": "/playable_portfolio/static/js/runtime-main.a727d610.js"
+  },
+  {
+    "revision": "e8dabeb14dd4aba8741f887541fd6419",
+    "url": "/playable_portfolio/static/media/Headshot.e8dabeb1.jpg"
+  }
+]);

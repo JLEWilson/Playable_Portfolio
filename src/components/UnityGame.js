cf13d59c469b8e801b19685a3b5bd582ade24111
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-07-05 13:19:06 -0800

    add loading percentage, update to use latest version of React-Unity WebGl

--- src/components/UnityGame.js
+++ src/components/UnityGame.js
@@ -1,19 +1,18 @@
 import React from "react";
-import Unity, { UnityContext } from "react-unity-webgl";
+import { Unity, useUnityContext } from "react-unity-webgl";
 import { useMediaQuery } from '../Hooks/MediaQuery';
 
-const unityContext = new UnityContext({
-  loaderUrl: "./unityBuild/Playable_Portfolio.loader.js",
-  dataUrl: "./unityBuild/Playable_Portfolio.data",
-  frameworkUrl: "./unityBuild/Playable_Portfolio.framework.js",
-  codeUrl: "./unityBuild/Playable_Portfolio.wasm",
-});
-
-function UnityGame() {
+const UnityGame = () => {
   const isMDScreen = useMediaQuery('(min-width: 800px) and (max-width: 1199px)');
   const isLGScreen = useMediaQuery('(min-width: 1200px) and (max-width: 1599px)');
   const isXLScreen = useMediaQuery('(min-width: 1600px)');
-  
+  const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
+    loaderUrl: "./unityBuild/Playable_Portfolio.loader.js",
+    dataUrl: "./unityBuild/Playable_Portfolio.data",
+    frameworkUrl: "./unityBuild/Playable_Portfolio.framework.js",
+    codeUrl: "./unityBuild/Playable_Portfolio.wasm",
+  });
+  const loadingPercentage = Math.round(loadingProgression * 100);
   const style = {
     game: (isMd, isLg, isXl) => ({
       width:  isXl ? "1280px" :
@@ -24,14 +23,34 @@ function UnityGame() {
               isMd ? "360px" :"180px",
       margin: "1em auto",
       display: "block"
-		})
-    
+		}),
+    loading: {
+      position: "absolute",
+      marginLeft: "auto",
+      marginRight: "auto",
+      left: "0",
+      right: "0",
+      top: "2.75em",
+      textAlign: "center",
+      color: "rgba(235, 250, 215, .80)",
+      fontWeight: "700",
+      fontSize: "5em",
+    }
   }
 
-  return <Unity 
-          style={style.game(isMDScreen, isLGScreen, isXLScreen)} 
-          unityContext={unityContext} />
-  
+  return (
+    <div >
+      {isLoaded === false && (
+      // We'll conditionally render the loading overlay if the Unity
+      // Application is not loaded.
+      <div style={style.loading}>
+        <p>Loading... ({loadingPercentage}%)</p>
+      </div>
+      )}
+      <Unity style={style.game(isMDScreen, isLGScreen, isXLScreen)}
+             unityProvider={unityProvider} />
+    </div>
+  )
 }
 
 export default UnityGame;
